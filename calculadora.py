9af7b43c6a313841cd331623023061b725fbbb6f
Stephanie Arismendi <stephaniearismendi@gmail.com>
2024-06-03 01:20:58 +0200

    add basic functions

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,19 @@
 import tkinter as tk
 
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        return "Error: División por cero"
+    return a / b
+
 class CalculadoraApp:
     def __init__(self, master):
         self.master = master
